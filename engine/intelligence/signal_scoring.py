3b2b33b5f51daac031c3b96c3da7931434ed3656
TechSolute <pharshiesammy@gmail.com>
2026-02-04 12:39:52 +0100

    Update signal_scoring.py

--- engine/intelligence/signal_scoring.py
+++ engine/intelligence/signal_scoring.py
@@ -1,1 +1,20 @@
 
+def score_signal(features):
+    """
+    features = dict of signal metrics
+    """
+    score = 0
+
+    if features.get("trend_strength", 0) > 0.7:
+        score += 30
+
+    if features.get("liquidity_sweep"):
+        score += 25
+
+    if features.get("rarity_score", 0) > 0.8:
+        score += 25
+
+    if features.get("probability", 0) > 0.75:
+        score += 20
+
+    return min(score, 100)
