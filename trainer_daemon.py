26cc6c0a436cfb2fd56819e41534ed9afc159721
TechSolute <pharshiesammy@gmail.com>
2026-02-05 22:08:32 +0100

    Update trainer_daemon.py

--- trainer_daemon.py
+++ trainer_daemon.py
@@ -1,9 +1,14 @@
 # trainer_daemon.py
 
 import sys, os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from engine.runner import run_engine
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(ROOT_DIR)
+
+from Nexus.engine.runner import run_engine
+
 
 if __name__ == "__main__":
+    print("🚀 Nexus Engine Booting...")
     run_engine()
+    print("✅ Nexus Engine Finished")
