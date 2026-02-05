caedf4fd5846ea0cad8071537da0aed95ce4e1e2
TechSolute <pharshiesammy@gmail.com>
2026-02-05 23:27:07 +0100

    Update trainer_daemon.py

--- trainer_daemon.py
+++ trainer_daemon.py
@@ -1,14 +1,11 @@
-# trainer_daemon.py
-
 import sys
 import os
 
-# Add project root to Python path
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, ROOT_DIR)
 
 from engine.runner import run_engine
 
-
 if __name__ == "__main__":
     run_engine()
+    print("✅ Nexus Engine finished")
